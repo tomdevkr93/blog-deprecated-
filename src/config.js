df1d801edd3d9c21d449e848d17b0d9b3da8dc2b
devho813 <devho813@gmail.com>
2020-08-14 11:36:25 +0900

    환경 설정

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "MWM2ZDMxY2M1OTc4YjZkN2EzZjRlNWI5NTc1YmQyYTE3NGM5ODhjNQ==",
+
+  // Your Github UserName
+  githubUserName: "devho813",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "blog",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'Iv1.2e136cdb6068cd8f',
+  OAuthSecret: '8c1886ce5de2dba0543469311b85e74b5f0d30f8',
+
+  // Your Personal Blog Title
+  title : "이찬호 개발 로그" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "🖥 Front-End Engineer",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
